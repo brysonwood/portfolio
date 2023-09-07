18b17a918b60c2b0f0288abb6eea12c4cdafc71f
Bryson Wood <mail@brysonwood.com>
2023-09-07 16:59:12 -0700

    Added typing animation for DOMContentLoaded

---
+++ js/writer.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',function(event){
+    var test = "Bryson Wood."
+
+    startWriterAnimation(0);
+
+    function writer(tester, i, fnCallback) {
+        if (i < (tester.length)) {
+            document.querySelector(".intro-writer h1").innerHTML = tester.substring(0, i + 1) +'<span aria-hidden="true"></span>'
+
+            setTimeout(function() {
+                writer(tester, i + 1, fnCallback)
+            }, 100);
+        }
+        else if (typeof fnCallback == 'function') {
+            setTimeout(fnCallback, 700);
+        }
+    }
+
+    function startWriterAnimation(i) {
+        if (i < 1) {
+            writer(test, 0, function(){
+                startWriterAnimation(i + 1);
+            });
+        }
+    }
+
+    startWriterAnimation(0);
+});
